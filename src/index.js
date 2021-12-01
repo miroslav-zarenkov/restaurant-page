295a27fbf680291c20511afc5f9dd80f8cd044a7
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-12-01 19:05:30 +0300

    Testing Webpack, learning

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import './style.css';
+
 console.log("Hello message");
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "Test Text";
+document.body.appendChild(content);
